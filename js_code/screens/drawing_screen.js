6bb53a878f26d835b63fd101e9837808446facc4
Josh Jarvis <ixonstater@gmail.com>
2019-03-04 10:09:10 -0700

    State changes for native component

--- js_code/screens/drawing_screen.js
+++ js_code/screens/drawing_screen.js
@@ -3,22 +3,41 @@ import {View, StyleSheet, Text} from 'react-native'
 import {NativeImage} from '../extras/native_drawing'
 
 
+
 export default class DrawingScreen extends Component{
   constructor(props){
     super(props)
+    this.state = {
+      colorIndex: 0,
+    }
+    setInterval(() => (
+      this.setState(function(previousState){
+        let newState = previousState
+        if(previousState.colorIndex == 2){
+          newState.colorIndex = 0
+        } else {
+          newState.colorIndex += 1
+        }
+        console.log(newState)
+        return newState
+      })
+    ), 1000)
   }
   render(){
     return(
-      <View style = {styles.bg}>
-        <NativeImage width = {'100%'} height = {'100%'} bgcolor = {20}></NativeImage>
+      <View style = {styles.wrapper}>
+        <NativeImage
+          width = {'100%'}
+          height = {'100%'}
+          bgcolor = {colors[this.state.newColor]}>
+        </NativeImage>
       </View>
     )
   }
 }
 const styles = StyleSheet.create({
-  bg: {
+  wrapper: {
     height: '100%',
     width: '100%',
-    backgroundColor: 'black',
   }
 })
