90f7827b4d2f48c3108a50f141faecafc61041c8
Josh Jarvis <ixonstater@gmail.com>
2019-03-10 14:24:13 -0600

    Basic line drawing functionality

--- js_code/extras/native_drawing.js
+++ js_code/extras/native_drawing.js
@@ -1,4 +1,4 @@
 import {NativeModules} from 'react-native'
 import {requireNativeComponent} from 'react-native'
 export const NativeDrawing = NativeModules.RNNativeDrawing
-export const NativeImage = requireNativeComponent('NativeImage')
+export const NativeTablet = requireNativeComponent('NativeTablet')
--- js_code/screens/drawing_screen.js
+++ js_code/screens/drawing_screen.js
@@ -1,33 +1,21 @@
 import React, {Component} from 'react'
 import {View, StyleSheet, Text} from 'react-native'
-import {NativeImage} from '../extras/native_drawing'
+import {NativeTablet} from '../extras/native_drawing'
 
 
 
 export default class DrawingScreen extends Component{
   constructor(props){
     super(props)
-    this.state = {coords: [0,0]}
-    setInterval(() => (
-      this.setState(function(previousState){
-        console.log("running in the loop")
-        if(previousState.coords[0] == 0){
-          return {coords: [500,500]}
-        }else{
-          return {coords: [0,0]}
-        }
-      })
-    ), 1000)
   }
   render(){
     return(
       <View style = {styles.wrapper}>
-        <NativeImage
+        <NativeTablet
           width = {'100%'}
           height = {'100%'}
-          bgcolor = {[200,200,200]}
-          triggerdraw = {this.state.coords}>
-        </NativeImage>
+          triggerdraw = {[0, 0, 17]}>
+        </NativeTablet>
       </View>
     )
   }
