60c122c77ea56909e1109c1d31f944cdcd8ca57b
Josh Jarvis <ixonstater@gmail.com>
2019-03-10 20:51:57 -0600

    More stuff...and Im gonna die from the homeworks :(

---
+++ js_code/extras/position_calculation.js
@@ -0,0 +1,12 @@
+
+export default class MotionTracker{
+    constructor(){
+        this.x = 50
+        this.y = 50
+        this.xVel = 0
+        this.yVel = 0
+    }
+    iterate(forceX, forceY){
+        
+    }
+}
