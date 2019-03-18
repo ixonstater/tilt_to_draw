380e5006d3546ec69a7b9fe5c86b8fa7a74fea64
Josh Jarvis <ixonstater@gmail.com>
2019-03-18 16:22:22 -0600

    Finished adding friction to the physics

--- js_code/extras/position_calculation.js
+++ js_code/extras/position_calculation.js
@@ -7,24 +7,64 @@ export default class MotionTracker{
         this.yOffset = midY / 50
         this.xVel = 0
         this.yVel = 0
+        this.friction = 1.2
+        this.maxXVel = 1.5
+        this.maxYVel = 1.5
     }
     update(data){
-        console.log(data.x)
-        let {x,y} = data
-        this.xVel += x * -0.03
-        this.yVel += y * 0.03
+        let {x, y} = data
+        let [xForce, yForce] = this.applyFriction(x,y)
+        console.log(xForce,yForce)
+        this.calcVelocity(xForce, yForce)
+        this.generateCoords()
+        return ([this.xOffset * this.xCoord, this.yOffset * this.yCoord])
+    }
 
-        if(this.xVel < -1){
-            this.xVel = -1
-        }else if(this.xVel > 1){
-            this.xVel = 1
+    applyFriction(x,y){
+        let tempx = 0
+        let tempy = 0
+        if(Math.abs(x) - this.friction < 0){
+            tempx = 0
+        }else if(this.isPositive(x)){
+            tempx = x - this.friction
+        }else{
+            tempx = x + this.friction
         }
-        if(this.yVel < -1){
-            this.yVel = -1
-        }else if(this.yVel > 1){
-            this.yVel = 1
+        if(Math.abs(y) - this.friction < 0){
+            tempy = 0
+        }else if(this.isPositive(y)){
+            tempy = y - this.friction
+        }else{
+            tempy = y + this.friction
+        }
+        return([tempx, tempy])
+    }
+
+    calcVelocity(xForce,yForce){
+        this.xVel += xForce * -0.03
+        this.yVel += yForce * 0.03
+
+        if(xForce == 0){
+            this.xVel /= 1.3
         }
 
+        if(yForce == 0){
+            this.yVel /= 1.3
+        }
+
+        if(this.xVel < -this.maxXVel){
+            this.xVel = -this.maxXVel
+        }else if(this.xVel > this.maxXVel){
+            this.xVel = this.maxXVel
+        }
+        if(this.yVel < -this.maxYVel){
+            this.yVel = -this.maxYVel
+        }else if(this.yVel > this.maxYVel){
+            this.yVel = this.maxYVel
+        }
+    }
+
+    generateCoords(){
         this.xCoord += this.xVel
         this.yCoord += this.yVel
 
@@ -42,6 +82,9 @@ export default class MotionTracker{
             this.yVel *= -1
             this.yCoord += 1
         }
-        return ([this.xOffset * this.xCoord, this.yOffset * this.yCoord])
+    }
+
+    isPositive(num){
+        return num > 0
     }
 }
